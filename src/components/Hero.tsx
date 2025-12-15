8d6da64dde1e48980ee917d0743f47b21253845d
richardborgesdev <richardborgesdev@gmail.com>
2025-12-15 09:27:35 -0300

    chore: tailwindcss | test avatar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,4 @@
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export function Hero() {
   return (
@@ -38,6 +34,22 @@ export function Hero() {
           <AvatarFallback>ER</AvatarFallback>
         </Avatar>
       </div>
+      <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
+        <img
+          className="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0"
+          src="https://avatars.githubusercontent.com/u/19519036?v=4"
+          alt=""
+        />
+        <div className="space-y-2 text-center sm:text-left">
+          <div className="space-y-0.5">
+            <p className="text-lg font-semibold text-black">Erin Lindford</p>
+            <p className="font-medium text-gray-500">Product Engineer</p>
+          </div>
+          <button className="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ...">
+            Message
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
