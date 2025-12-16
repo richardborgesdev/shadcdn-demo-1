21305e5f8e0ebd07f4ac28aa21a8938dd4e02455
richardborgesdev <richardborgesdev@gmail.com>
2025-12-16 10:08:21 -0300

    fix: design | avatar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,39 +1,6 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-
 export function Hero() {
   return (
     <div className="flex flex-row flex-wrap items-center gap-12">
-      <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-      <Avatar className="rounded-lg">
-        <AvatarImage
-          src="https://github.com/evilrabbit.png"
-          alt="@evilrabbit"
-        />
-        <AvatarFallback>ER</AvatarFallback>
-      </Avatar>
-      <div className="*:data-[slot=avatar]:ring-background flex -space-x-2 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:grayscale">
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-        <Avatar>
-          <AvatarImage
-            src="https://github.com/maxleiter.png"
-            alt="@maxleiter"
-          />
-          <AvatarFallback>LR</AvatarFallback>
-        </Avatar>
-        <Avatar>
-          <AvatarImage
-            src="https://github.com/evilrabbit.png"
-            alt="@evilrabbit"
-          />
-          <AvatarFallback>ER</AvatarFallback>
-        </Avatar>
-      </div>
       <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
         <img
           className="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0"
@@ -42,12 +9,9 @@ export function Hero() {
         />
         <div className="space-y-2 text-center sm:text-left">
           <div className="space-y-0.5">
-            <p className="text-lg font-semibold text-black">Erin Lindford</p>
-            <p className="font-medium text-gray-500">Product Engineer</p>
+            <p className="text-lg font-semibold text-black">Richard Borges</p>
+            <p className="font-medium text-gray-500">Frontend Architect | UX Engineering</p>
           </div>
-          <button className="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ...">
-            Message
-          </button>
         </div>
       </div>
     </div>
